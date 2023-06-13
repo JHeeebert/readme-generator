79015ecb54b62aa821b1ad8e49c3f808bf7ed384
Joshua Hebert <jhebertcocktails@gmail.com>
2023-06-12 22:30:02 -0400

    index,js & generatemarkdown,js

--- index.js
+++ index.js
@@ -1,13 +1,82 @@
-// TODO: Include packages needed for this application
+// Packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'userName',
+        message: 'What is your GitHub username?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?'
+    },
+    {
+        type: 'input',
+        name: 'projectName',
+        message: 'What is your project\'s name?'
+    },
+    {
+        type: 'input',
+        name: 'projectDescription',
+        message: 'Please write a short description of your project:'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the steps required to install your project?'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide instructions and examples for use:'
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'What are the steps required to contribute to your project?'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Provide examples on how to run tests:'
+    },
+    {
+      type: 'list',
+        name: 'license',
+        message: 'What kind of license should your project have?',
+        choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None']
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+            return console.log(err);
+        }
+        console.log('Success!');
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+        .then(answers => {
+            writeToFile('README.md', generateMarkdown(answers));
+        });
+}
 
+// Function call to initialize program
+function init() {
+    inquirer.prompt(questions)
+        .then(answers => {
+            writeToFile('README.md', generateMarkdown(answers));
+        });
+};
 // Function call to initialize app
 init();
